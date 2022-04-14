b5078029ddfbb2bd56103f710e6f71b23a3a2d12
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2022-04-14 09:39:11 +0300

    string kicsi maganhangzo utan nagy is, szimplan

---
+++ 2022_04_14_lab_40/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string szo, szo2;
+    cin >> szo;
+    int pos= 'a'- 'A';
+    for(int j=0; j<szo.size(); j++){
+        if(szo[j]=='a' || szo[j]=='e' || szo[j]=='i' || szo[j]=='o' || szo[j]=='u'){
+           szo2+=szo[j];
+           szo2+=(szo[j]-pos);
+        }else{
+           szo2+=szo[j];
+        }
+    }
+    cout << szo2;
+    return 0;
+}
